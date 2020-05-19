3610714f617b70c9bce78421683759774da46a4c
Sara Garcia <sarisgar28@gmail.com>
2020-05-19 15:54:03 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,7 @@ class Student
   
   def self.drop_table 
       sql = <<-SQL
-      DROP TABLE [IF EXISTS] students(
+      DROP TABLE IF EXISTS students(
         id INTEGER PRIMARY KEY,
         name TEXT, 
        grade TEXT

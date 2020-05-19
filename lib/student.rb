fbfd72ce68d64a6b218351130c285542d0577e6b
Sara Garcia <sarisgar28@gmail.com>
2020-05-19 15:57:15 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_accessor :name, :grade 
   attr_reader :id
 
-  def initialize(name,grade,id = nil)
+  def initialize(name,grade,id=nil)
     @name = name 
     @grade = grade 
     @id = id 
@@ -20,16 +20,16 @@ class Student
     DB[:conn].execute(sql)
   end 
   
-  def self.drop_table 
-      sql = <<-SQL
-      DROP TABLE IF EXISTS students(
-        id INTEGER PRIMARY KEY,
-        name TEXT, 
-       grade TEXT
-      ) 
-      SQL 
-      DB[:conn].execute(sql)  
-  end 
+  #def self.drop_table 
+     # sql = <<-SQL
+      #DROP TABLE IF EXISTS students(
+       # id INTEGER PRIMARY KEY,
+       # name TEXT, 
+      # grade TEXT
+      #) 
+      #SQL 
+      #DB[:conn].execute(sql)  
+ # end 
 
 
   def save 
@@ -37,7 +37,6 @@ class Student
     INSERT INTO students (name, grade)
     VALUES (?, ?)
     SQL 
-
     DB[:conn].execute(sql, self.name,self.grade)
  end 
 

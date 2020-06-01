4cdfc3909c0b88717f16f22bdc5b789d95910b67
VQuine <01010110.v@gmail.com>
2020-06-01 13:18:33 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -10,10 +10,13 @@ class Student
 
   def self.create_table
     sql = <<-SQL
+    
     CREATE TABLE IF NOT EXISTS students (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    grade INTEGER
+
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+
     )
     SQL
 
